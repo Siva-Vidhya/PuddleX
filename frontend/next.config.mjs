9acd98ab5b4676c009cf18038d03a199ea461132
Sivavidhya P <2005sivavidhya@gmail.com>
2026-09-06 19:16:29 +0530

    Phase 7 - frontend prepared for Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "localhost",
+      "puddlex-backend.onrender.com",
+      "tile.openstreetmap.org"
+    ]
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
+  }
+}
+export default nextConfig
